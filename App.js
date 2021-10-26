069ff83f70f10c0be411dc06d08dcd0139259197
DineshUIdesign <93181645+DineshUIdesign@users.noreply.github.com>
2021-10-26 22:36:53 +0530

    files added

---
+++ App.js
@@ -0,0 +1,16 @@
+import componentsScreen from "./src/Screens/ComponentsScreens";
+
+const componentsScreen = () => {
+  return <Text style={styles.textStyle}>This is the components Screen </Text>;
+  };
+  
+  
+  const styles =  StyleSheet.create ({
+  textStyle: {
+      fontSize: 30
+  }
+  
+  });
+  
+  
+  export default componentsScreen;
---
+++ src/Screens/ComponentsScreens.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Text, stylesheet, View } from 'react-native';
+
+const componentsScreen = () => {
+return (
+<view>
+    <Text style={styles.textStyle}>This is the components Screen </Text>
+    <text>hello world</text>
+    </view>
+);
+};
+
+
+const styles =  StyleSheet.create ({
+textStyle: {
+    fontSize: 30
+}
+
+});
+
+
+export default componentsScreen; 
