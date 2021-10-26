5374941b178f4e3387f6d931d53a94504c05f8e6
DineshUIdesign <dineshkumar.m@foodhub.com>
2021-10-26 22:57:31 +0530

    edit

--- src/Screens/ComponentsScreens.js
+++ src/Screens/ComponentsScreens.js
@@ -2,21 +2,21 @@ import React from 'react'
 import { Text, stylesheet, View } from 'react-native';
 
 const componentsScreen = () => {
-return (
-<view>
-    <Text style={styles.textStyle}>This is the components Screen </Text>
-    <text>hello world</text>
-    </view>
-);
+    return (
+        <view>
+            <Text style={styles.textStyle}>This is the components Screen </Text>
+            <text>hello world</text>
+        </view>
+    );
 };
 
 
-const styles =  StyleSheet.create ({
-textStyle: {
-    fontSize: 30
-}
+const styles = StyleSheet.create({
+    textStyle: {
+        fontSize: 30
+    }
 
 });
 
 
-export default componentsScreen; 
+export default componentsScreen;
